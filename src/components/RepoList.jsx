2a8c9372f9372b44e432e2af54ecb757b8a9d555
Alan <alanross2006@gmail.com>
2021-08-30 21:05:41 +0100

    Tidy up template and initialize app skeleton

---
+++ src/components/RepoList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function RepoList() {}
+
+export default RepoList;
---
+++ src/screens/RepoListPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RepoListPage() {
+  return <h1>Github Repo List</h1>;
+}
+
+export default RepoListPage;
